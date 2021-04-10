e0e856722c274f5df893a9fedd29db41435e4fa0
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-10 17:20:04 +0000

    Delete build.sh

--- examples/async_monitor.rs
+++ examples/async_monitor.rs
@@ -1,1 +1,6 @@
-use futu
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher, Config};
+use std::path::Path;
