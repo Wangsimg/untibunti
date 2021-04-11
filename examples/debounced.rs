9da0bf177358110f7485ca4b35aa870f069ee463
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-11 20:39:06 +0000

    Adding some tests

--- examples/debounced.rs
+++ examples/debounced.rs
@@ -1,1 +1,8 @@
-use std::{path::Path, time::Du
+use std::{path::Path, time::Duration};
+
+use notify::{RecursiveMode};
+use notify_debouncer_mini::new_debouncer;
+
+/// Example for debouncer
+fn main() {
+    // emit some events by changing a file
