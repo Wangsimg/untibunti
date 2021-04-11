ae2ba35d546ccbc25d83dc3b41e03de6ac272144
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-11 06:56:50 +0000

    Create REQUIRE

--- examples/async_monitor.rs
+++ examples/async_monitor.rs
@@ -4,3 +4,12 @@ use futures::{
 };
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher, Config};
 use std::path::Path;
+
+/// Async, futures channel based event watching
+fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .expect("Argument 1 needs to be a path");
+    println!("watching {}", path);
+
+    futures::ex
