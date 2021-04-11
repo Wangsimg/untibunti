13805f476f20d5445102bf17119c3d6cafb2d5c5
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-11 21:20:28 +0000

    more working

--- examples/debounced.rs
+++ examples/debounced.rs
@@ -6,3 +6,8 @@ use notify_debouncer_mini::new_debouncer;
 /// Example for debouncer
 fn main() {
     // emit some events by changing a file
+    std::thread::spawn(|| {
+        let path = Path::new("test.txt");
+        let _ = std::fs::remove_file(&path);
+        loop {
+            std::fs::write(&path, b"Lorem ips
