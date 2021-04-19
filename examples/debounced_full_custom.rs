4bd8d2f5d9ab8b3c7e3b4d4f30c4c855fa393e2f
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-19 20:49:02 +0000

    LockD: pass service to per-net up and down functions

--- examples/debounced_full_custom.rs
+++ examples/debounced_full_custom.rs
@@ -5,4 +5,9 @@ use notify_debouncer_mini::new_debouncer_opt;
 
 /// Debouncer with custom backend and waiting for exit
 fn main() {
-    
+    // emit some events by changing a file
+    std::thread::spawn(|| {
+        let path = Path::new("test.txt");
+        let _ = std::fs::remove_file(&path);
+        loop {
+            std::
