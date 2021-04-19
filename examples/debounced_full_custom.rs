832708fc09c68b3f2d572f5d27a27a42bd2ead8c
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-19 13:20:35 +0000

    EmailtoTracScript:

--- examples/debounced_full_custom.rs
+++ examples/debounced_full_custom.rs
@@ -1,1 +1,8 @@
-u
+use std::{path::Path, time::Duration};
+
+use notify::{RecursiveMode, Config};
+use notify_debouncer_mini::new_debouncer_opt;
+
+/// Debouncer with custom backend and waiting for exit
+fn main() {
+    
