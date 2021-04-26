3bfb2952ca7e445a2679d2a55cd6a67d382af532
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-26 09:08:38 +0000

    Update build tools version

--- examples/hot_reload_tide/src/messages.rs
+++ examples/hot_reload_tide/src/messages.rs
@@ -4,4 +4,16 @@ use std::collections::HashMap;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Config {
     pub audio_folder_path: String,
-    pub messag
+    pub messages: Messages,
+}
+
+/// The key is the audio file name
+type Messages = HashMap<String, Message>;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Message {
+    pub display_name: String,
+    pub volume: f32,
+}
+
+pub fn load_co
