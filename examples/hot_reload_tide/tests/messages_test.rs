8fe8c418c5cfae9eb6644c905e8ae8d615958235
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-27 11:16:33 +0000

    add samples

--- examples/hot_reload_tide/tests/messages_test.rs
+++ examples/hot_reload_tide/tests/messages_test.rs
@@ -11,4 +11,5 @@ fn load_config_from_file() {
 
     let message = messages.get("sound.mp3").unwrap();
 
-    assert_eq!(message.display_name, "Sound 1")
+    assert_eq!(message.display_name, "Sound 1");
+}
