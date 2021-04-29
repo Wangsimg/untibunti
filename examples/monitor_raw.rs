a9b72f46715b099d9ab9d6f77f09f78e240a9121
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-04-29 12:01:16 +0000

    spelling mistake

--- examples/monitor_raw.rs
+++ examples/monitor_raw.rs
@@ -1,1 +1,8 @@
-use notify::{RecommendedWatcher, Rec
+use notify::{RecommendedWatcher, RecursiveMode, Watcher, Config};
+use std::path::Path;
+
+fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .expect("Argument 1 needs to be a path");
+    println!("watching {}"
