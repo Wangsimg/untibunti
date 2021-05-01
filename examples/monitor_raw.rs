c7a3cb433f0d9409bb2fd99d6153d3aa078b8153
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-05-01 09:12:31 +0000

    f2fs: avoid build warning

--- examples/monitor_raw.rs
+++ examples/monitor_raw.rs
@@ -5,4 +5,11 @@ fn main() {
     let path = std::env::args()
         .nth(1)
         .expect("Argument 1 needs to be a path");
-    println!("watching {}"
+    println!("watching {}", path);
+    if let Err(e) = watch(path) {
+        println!("error: {:?}", e)
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mp
