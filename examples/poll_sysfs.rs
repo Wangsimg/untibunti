0a5602c831525f2636637d769fafebadcd17bd5a
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-05-02 11:42:36 +0000

    Remove unneeded code

--- examples/poll_sysfs.rs
+++ examples/poll_sysfs.rs
@@ -1,2 +1,6 @@
 /// Example for watching kernel internal filesystems like `/sys` and `/proc`
-/// These can't be watched by the default backend or unconfigured pollwatc
+/// These can't be watched by the default backend or unconfigured pollwatcher
+/// This example can't be demonstrated under windows, it might be relevant for network shares
+#[cfg(not(target_os = "windows"))]
+fn not_windows_main() -> notify::Result<()> {
+    use notify::{Pol
