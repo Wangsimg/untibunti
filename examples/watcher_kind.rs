56a6355764d538e1d31fb750bbc213bd1a04fbe5
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-05-12 10:24:36 +0000

    2.8.4

--- examples/watcher_kind.rs
+++ examples/watcher_kind.rs
@@ -4,4 +4,7 @@ use notify::*;
 // example of detecting the recommended watcher kind
 fn main() {
     let (tx, rx) = std::sync::mpsc::channel();
-    // This example is a little bit misleading as you can just create one Config and use it for all watch
+    // This example is a little bit misleading as you can just create one Config and use it for all watchers.
+    // That way the pollwatcher specific stuff is still configured, if it should be used.
+    let mut watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
+        // c
