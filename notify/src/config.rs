ca29c4fad268b53147f448e333d9210c0be54e0e
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-06-12 19:15:21 +0000

    Folder structure

--- notify/src/config.rs
+++ notify/src/config.rs
@@ -2,4 +2,7 @@
 
 use std::time::Duration;
 
-/// Indicates whether only the provided directory or its sub-directories as
+/// Indicates whether only the provided directory or its sub-directories as well should be watched
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+pub enum RecursiveMode {
+    /// Watch all sub-directories as well, including directories c
