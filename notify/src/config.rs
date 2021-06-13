c4be90cb113fb90e05aef0778de32617df46878c
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-06-13 09:40:24 +0000

    initial codebase

--- notify/src/config.rs
+++ notify/src/config.rs
@@ -5,4 +5,12 @@ use std::time::Duration;
 /// Indicates whether only the provided directory or its sub-directories as well should be watched
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
 pub enum RecursiveMode {
-    /// Watch all sub-directories as well, including directories c
+    /// Watch all sub-directories as well, including directories created after installing the watch
+    Recursive,
+
+    /// Watch only the provided directory
+    NonRecursive,
+}
+
+impl RecursiveMode {
+    pub(crate) fn is_recursive(&self) -
