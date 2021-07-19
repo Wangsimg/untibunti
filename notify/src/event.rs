73efd51eb5324bdb39a3cdc7c20c3dd3b951bcd5
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-07-19 22:28:52 +0000

    automated merge

--- notify/src/event.rs
+++ notify/src/event.rs
@@ -1,1 +1,3 @@
-// T
+// This file is dual-licensed under the Artistic License 2.0 as per the
+// LICENSE.ARTISTIC file, and the Creative Commons Zero 1.0 license.
+//! The `Event` type and the hierarchical `Eve
