3b3333731241eabf3880de5b0c19e57d1a057df1
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2021-07-24 14:45:09 +0000

    mm: vmscan: clear kswapd's special reclaim powers before exiting

--- notify/src/event.rs
+++ notify/src/event.rs
@@ -1,3 +1,14 @@
 // This file is dual-licensed under the Artistic License 2.0 as per the
 // LICENSE.ARTISTIC file, and the Creative Commons Zero 1.0 license.
-//! The `Event` type and the hierarchical `Eve
+//! The `Event` type and the hierarchical `EventKind` descriptor.
+
+use std::{
+    fmt,
+    hash::{Hash, Hasher},
+    path::PathBuf,
+};
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+/// An event describing open or close operat
