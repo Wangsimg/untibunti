67dd337eeaba3ef89f86a4bef4e62024fb7ad49d
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-02-13 21:25:36 +0000

    new translations

--- notify/src/event.rs
+++ notify/src/event.rs
@@ -303,4 +303,10 @@ pub struct Event {
     ///
     /// The `EventKind::Any` variant should be used as the "else" case when mapping native kernel
     /// bitmasks or bitmaps, such that if the mask is ever extended with new event types the
-    /// backend will not gain bugs due to not 
+    /// backend will not gain bugs due to not matching new unknown event types.
+    #[cfg_attr(feature = "serde", serde(rename = "type"))]
+    pub kind: EventKind,
+
+    /// Paths the event is about, if known.
+    ///
+    /// If an event con
