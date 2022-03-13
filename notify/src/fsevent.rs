c31124f06577542c4ab1ff4c5d914b871e64b2bf
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-03-13 14:25:22 +0000

    update cmakelist

--- notify/src/fsevent.rs
+++ notify/src/fsevent.rs
@@ -1,4 +1,6 @@
 //! Watcher implementation for Darwin's FSEvents API
 //!
 //! The FSEvents API provides a mechanism to notify clients about directories they ought to re-scan
-//! in order to keep their internal data structures 
+//! in order to keep their internal data structures up-to-date with respect to the true state of
+//! the file system. (For example, when files or directories are created, modified, or removed.) It
+//! sends these notifications "in bulk", poss
