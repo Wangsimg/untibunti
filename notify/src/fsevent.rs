34fb5b399de050587b453584bc16ac0b038801fd
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-03-13 18:32:57 +0000

    Upgrade to bundles-pom 13

--- notify/src/fsevent.rs
+++ notify/src/fsevent.rs
@@ -3,4 +3,9 @@
 //! The FSEvents API provides a mechanism to notify clients about directories they ought to re-scan
 //! in order to keep their internal data structures up-to-date with respect to the true state of
 //! the file system. (For example, when files or directories are created, modified, or removed.) It
-//! sends these notifications "in bulk", poss
+//! sends these notifications "in bulk", possibly notifying the client of changes to several
+//! directories in a single callback.
+//!
+//! For more information see the [FSEvents API reference][ref].
+//!
+//! TODO: doc
