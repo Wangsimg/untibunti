39ded29bf38a927d35491485397870b94a5e7f92
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-03-13 13:12:59 +0000

    Milestone 1

--- notify/src/fsevent.rs
+++ notify/src/fsevent.rs
@@ -1,1 +1,4 @@
-//! Watcher implementation for 
+//! Watcher implementation for Darwin's FSEvents API
+//!
+//! The FSEvents API provides a mechanism to notify clients about directories they ought to re-scan
+//! in order to keep their internal data structures 
