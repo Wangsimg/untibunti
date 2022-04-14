2f4bcc250bd4b6c7c84389a543bc5d2def982ff8
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-04-14 15:21:24 +0000

    remove empty files

--- notify/src/fsevent.rs
+++ notify/src/fsevent.rs
@@ -343,4 +343,9 @@ impl FsEventWatcher {
 
             cf::CFRelease(cf_path);
 
-            for idx in to_remove.i
+            for idx in to_remove.iter().rev() {
+                cf::CFArrayRemoveValueAtIndex(self.paths, *idx);
+            }
+        }
+        let p = if let Ok(canonicalized_path) = path.canonicalize() {
+            canon
