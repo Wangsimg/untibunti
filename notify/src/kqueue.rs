6ae736133872a5a75443486f4acddda9ab133ecf
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-05-22 11:22:13 +0000

    more ui changes

--- notify/src/kqueue.rs
+++ notify/src/kqueue.rs
@@ -1,1 +1,5 @@
-//! Watcher implementation for
+//! Watcher implementation for the kqueue API
+//!
+//! The kqueue() system call provides a generic method of notifying the user
+//! when an event happens or a condition holds, based on the results of small
+//! pieces of ke
