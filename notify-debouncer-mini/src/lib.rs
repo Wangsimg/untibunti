91b61c7682d876c19a139892a42a8c02abcb49d4
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2022-10-08 12:29:02 +0000

    Prepare 0.8.2 release

--- notify-debouncer-mini/src/lib.rs
+++ notify-debouncer-mini/src/lib.rs
@@ -10,4 +10,13 @@
 //! specify notify as dependency explicitely in your dependencies.
 //! Otherwise you can just use the re-export of notify from debouncer-mini.
 //! ```toml
-/
+//! notify-debouncer-mini = "0.2.0"
+//! notify = { version = "..", features = [".."] }
+//! ```
+//!  
+//! # Examples
+//!
+//! ```rust,no_run
+//! # use std::path::Path;
+//! # use std::time::Duration;
+//! use notify_debouncer_mini::{notify::*
