599dc96711b26f981e6d43e481e567d4804bb937
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2023-01-15 16:02:38 +0000

    commit message

--- tests/race-with-remove-dir.rs
+++ tests/race-with-remove-dir.rs
@@ -1,1 +1,6 @@
-use std::
+use std::{fs, thread, time::Duration};
+
+use notify::{RecursiveMode, Watcher};
+
+/// Test for <https://github.com/notify-rs/notify/issues/301>.
+/// Note: This test will fail if your temp directory is not writable.
