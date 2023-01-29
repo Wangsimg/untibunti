196bc45ce7de5b1ff30a50aa590b9eccd29f5f74
Wangsimg <119611665+Wangsimg@users.noreply.github.com>
2023-01-29 07:46:17 +0000

    Update core

--- tests/race-with-remove-dir.rs
+++ tests/race-with-remove-dir.rs
@@ -4,3 +4,11 @@ use notify::{RecursiveMode, Watcher};
 
 /// Test for <https://github.com/notify-rs/notify/issues/301>.
 /// Note: This test will fail if your temp directory is not writable.
+#[test]
+fn test_race_with_remove_dir() {
+    let tmpdir = tempfile::tempdir().unwrap();
+
+    {
+        let tmpdir = tmpdir.path().to_path_buf();
+        let _ = thread::Builder::new()
+            .name("noti
